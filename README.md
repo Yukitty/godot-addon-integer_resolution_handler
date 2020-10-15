36d7fa46fd37d6ea9140959de8d57c45640777d0
Yukitty <catgirl@goddess.moe>
2020-10-15 11:36:59 -0400

    Better handling of project settings
    No longer overrides your settings on project load,
    and gives settings a fixed ordering
    relative to a related engine setting.

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ This addon is a complete runtime script for handling integer resolutions for pix
 ## Usage
 
 1. Enable the plugin. Close Project Settings.
-2. Navigate Project Settings to the `display/window` category and scroll to the bottom.
+2. Navigate Project Settings to the `display/window` category.
 3. In the new section "Integer Resolution Handler", set Base Width and Base Height to your game's native pixel resolution.
 
 The IntegerResolutionHandler also works with all of the existing `stretch` settings, so fiddle there if you don't like how it behaves. Notably, setting `stretch/aspect` to "Keep" will enforce strict screen resolutions, while "Expand" will allow the viewable area to nearly double between scale steps.
--- addons/integer_resolution_handler/plugin.gd
+++ addons/integer_resolution_handler/plugin.gd
@@ -2,32 +2,38 @@ tool
 extends EditorPlugin
 
 
+const SETTING_BASE_WIDTH := "display/window/integer_resolution_handler/base_width"
+const SETTING_BASE_HEIGHT := "display/window/integer_resolution_handler/base_height"
+const DEFAULT_BASE_WIDTH: int = 320
+const DEFAULT_BASE_HEIGHT: int = 240
+
+
 func _enter_tree():
 	add_autoload_singleton("IntegerResolutionHandler", "res://addons/integer_resolution_handler/integer_resolution_handler.gd")
 
-	ProjectSettings.set_setting(
-		"display/window/integer_resolution_handler/base_width",
-		max(floor(ProjectSettings.get_setting("display/window/size/width") / 3), 8))
+	if not ProjectSettings.has_setting(SETTING_BASE_WIDTH):
+		ProjectSettings.set_setting(SETTING_BASE_WIDTH, DEFAULT_BASE_WIDTH)
+	ProjectSettings.set_initial_value(SETTING_BASE_WIDTH, DEFAULT_BASE_WIDTH)
 	ProjectSettings.add_property_info({
-		"name": "display/window/integer_resolution_handler/base_width",
+		"name": SETTING_BASE_WIDTH,
 		"type": TYPE_INT,
 		"hint": PROPERTY_HINT_RANGE,
 		"hint_string": "1,1024,1,or_greater"
 	})
-	ProjectSettings.set_initial_value(
-		"display/window/integer_resolution_handler/base_width", 320)
 
-	ProjectSettings.set_setting(
-		"display/window/integer_resolution_handler/base_height",
-		max(floor(ProjectSettings.get_setting("display/window/size/height") / 3), 8))
+	if not ProjectSettings.has_setting(SETTING_BASE_HEIGHT):
+		ProjectSettings.set_setting(SETTING_BASE_HEIGHT, DEFAULT_BASE_HEIGHT)
+	ProjectSettings.set_initial_value(SETTING_BASE_HEIGHT, DEFAULT_BASE_HEIGHT)
 	ProjectSettings.add_property_info({
-		"name": "display/window/integer_resolution_handler/base_height",
+		"name": SETTING_BASE_HEIGHT,
 		"type": TYPE_INT,
 		"hint": PROPERTY_HINT_RANGE,
 		"hint_string": "1,600,1,or_greater"
 	})
-	ProjectSettings.set_initial_value(
-		"display/window/integer_resolution_handler/base_height", 240)
+
+	var order: int = ProjectSettings.get_order("display/window/size/width") - 2
+	ProjectSettings.set_order(SETTING_BASE_WIDTH, order)
+	ProjectSettings.set_order(SETTING_BASE_HEIGHT, order + 1)
 
 
 func _exit_tree():
